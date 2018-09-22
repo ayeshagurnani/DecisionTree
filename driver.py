a8a187d733b5f7e922dd222944e58c297b54b1d7
Ayesha Gurnani <ayeshagurnani1302@gmail.com>
2018-09-22 10:38:45 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -2,52 +2,40 @@ from DecisionTree import *
 import pandas as pd
 from sklearn import model_selection
 
-header = ['SepalL', 'SepalW', 'PetalL', 'PetalW', 'Class']
-#training_data =  [
-#    ['Green', 3, 'Apple'],
-#    ['Yellow', 3, 'Apple'],
- #   ['Red', 1, 'Grape'],
- #   ['Red', 1, 'Grape'],
- #   ['Yellow', 3, 'Lemon'],
-#]
-
-df = pd.read_csv('iris.csv', header = None, names=['SepalL','SepalW','PetalL','PetalW','Class'])
+#We have used checked the accuracy on two datasets.
+header_tic = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'Class'] #First one is the tic_tac_toe dataset.
+header_leaves = ['SepalL', 'SepalW', 'PetalL', 'PetalW', 'Class'] #Another is the iris dataset.
+header = header_tic
+df = pd.read_csv('tic_tac_toe.csv', header=None, names= header)
 lst = df.values.tolist()
-t = build_tree(lst, header)
-#print(t.true_branch.true_branch.class_label)
-#p = print_leaf(classify(training_data[0], t))
-#print(p)
-
-#inner = getInnerNodes(t)
-#print(inner)
-#leaf = getLeafNodes(t)
-#print(leaf)
-#p = computeAccuracy(lst,t)
-#print(p)
-#print("********** Leaf nodes ****************")
-#leaves = getLeafNodes(t)
-#print(len(leaves))
-#for leaf in leaves:
-#    print("id = " + str(leaf.id) + " depth =" + str(leaf.depth))
-#print("********** Non-leaf nodes ****************")
-#innerNodes = getInnerNodes(t)
-#for inner in innerNodes:
-#    print("id = " + str(inner.id) + " depth =" + str(inner.depth))
-
-#trainDF, testDF = model_selection.train_test_split(df, test_size=0.2)
-#train = trainDF.values.tolist()
-#test = testDF.values.tolist()
-
-#t = build_tree(train, header)
-#print("*************Tree before pruning*******")
+#t = build_tree(lst, header)
 #print_tree(t)
-#acc = computeAccuracy(test, t)
-#print("Accuracy on test = " + str(acc))
+
+trainDF, testDF = model_selection.train_test_split(df, test_size=0.3, random_state=1)
+train = trainDF.values.tolist()
+test = testDF.values.tolist()
+
+tree = build_tree(train, header)
+
+print("********** Leaf nodes ****************")
+leaves = getLeafNodes(tree)
+for leaf in leaves:
+    print("id = " + str(leaf.id) + " depth =" + str(leaf.depth))
+print("********** Non-leaf nodes ****************")
+innerNodes = getInnerNodes(tree)
+for inner in innerNodes:
+    print("id = " + str(inner.id) + " depth =" + str(inner.depth))
+
+
+print("*************Tree before pruning*******")
+#print_tree(tree)
+acc = computeAccuracy(test, tree)
+print("Accuracy on test = " + str(acc))
 
 ## TODO: You have to decide on a pruning strategy
-#t_pruned = prune_tree(t, [26, 11, 5])
-
-#print("*************Tree after pruning*******")
+t_pruned = prune_tree(tree, [0])
 #print_tree(t_pruned)
-#acc = computeAccuracy(train, t)
-#print("Accuracy on train = " + str(acc))
+print("*************Tree after pruning*******")
+#print_tree(t_pruned)
+acc1 = computeAccuracy(test, t_pruned)
+print("Accuracy on test = " + str(acc1))
